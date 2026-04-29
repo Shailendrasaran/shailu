02ddf2e39b5391aa07f79a1208339fbf1b80ae8e
Shailendrasaran <shailendrasaran.28csb@licet.ac.in>
2026-04-29 07:24:50 +0530

    Add files via upload

---
+++ New folder/model.js
@@ -0,0 +1,6 @@
+function checkUser(username, password) {
+    const correctUsername = "admin";
+    const correctPassword = "1234";
+
+    return username === correctUsername && password === correctPassword;
+}
