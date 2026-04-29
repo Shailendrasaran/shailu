22f9c235a2396abb2b234cf2b377b51f2ab4ccfc
Shailendrasaran <shailendrasaran.28csb@licet.ac.in>
2026-04-29 08:15:30 +0530

    Update and rename model.js to model.sql

--- New folder/model.js
+++
@@ -1,6 +0,0 @@
-function checkUser(username, password) {
-    const correctUsername = "admin";
-    const correctPassword = "1234";
-
-    return username === correctUsername && password === correctPassword;
-}
